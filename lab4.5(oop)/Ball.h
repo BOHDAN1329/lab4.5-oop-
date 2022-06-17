c26f82b57b6e8d4d3452fbefd6e5d9b7870273eb
BOHDAN1329 <90975949+BOHDAN1329@users.noreply.github.com>
2022-06-17 14:15:27 +0300

    Add files via upload

---
+++ lab4.5(oop)/Ball.h
@@ -0,0 +1,16 @@
+/////////////////
+//Ball.h
+#pragma once
+#include "Body.h"
+class Ball :
+	public Body
+{
+public:
+	Ball(double value);
+	virtual ~Ball() {};
+	virtual double Volume() override;
+	virtual double Surface() override;
+	virtual void Display();
+	virtual void Read();
+};
+
